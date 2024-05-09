8f98651c09659bba32332808c1a61117861e2f87
ItsDimitrie <laue0002@HZ.nl>
2024-05-09 16:07:48 +0200

    Added better navigation bar and footer

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,25 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import logo from '../../assets/DlLogoWit.png'
 import contactImg from '../../assets/contactMe.png'
 import {Link} from 'react-scroll';
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false)
+
   return (
     <nav className="navbar">
-      <img src={logo} alt="Logo" className='logo' />
+      <a href='/'><img src={logo} alt="Logo" className='logo' /></a>
 
       <div className="desktopMenu">
-        <Link className="dekstopMenuListItem">Home</Link>
-        <Link className="dekstopMenuListItem">About</Link>
-        <Link className="dekstopMenuListItem">Portfolio</Link>
-        <Link className="dekstopMenuListItem">Clients</Link>
+        <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="dekstopMenuListItem">Home</Link>
+        <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500}  className="dekstopMenuListItem">About</Link>
+        <Link activeClass='active' to='portfolio' spy={true} smooth={true} offset={-50} duration={500}  className="dekstopMenuListItem">Portfolio</Link>
+        <Link activeClass='active' to='clients' spy={true} smooth={true} offset={0} duration={500}  className="dekstopMenuListItem">Clients</Link>
       </div>
 
-      <button className="dekstopMenuBtn">
+      <button className="dekstopMenuBtn" onClick={() => {
+        document.getElementById('contact').scrollIntoView({behavior: 'smooth'})
+      }}>
       <i id='contactMeImg' class="fa-brands fa-whatsapp fa-2xl"></i>Contact Me</button>
 
-</nav>
+      <i id='mobMenu' class="fa-solid fa-bars fa-2xl" onClick={()=>setShowMenu(!showMenu)}></i>
+      <div className="navMenu" style={{display: showMenu? 'flex':'none'}}>
+        <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="ListItem" onClick={()=>setShowMenu(false)}>Home</Link>
+        <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500}  className="ListItem" onClick={()=>setShowMenu(false)}>About</Link>
+        <Link activeClass='active' to='portfolio' spy={true} smooth={true} offset={-50} duration={500}  className="ListItem" onClick={()=>setShowMenu(false)}>Portfolio</Link>
+        <Link activeClass='active' to='clients' spy={true} smooth={true} offset={0} duration={500}  className="ListItem" onClick={()=>setShowMenu(false)}>Clients</Link>
+        <Link activeClass='active' to='contact' spy={true} smooth={true} offset={0} duration={500}  className="ListItem" onClick={()=>setShowMenu(false)}>Contact</Link>
+      </div>
+  </nav>
   )
 }
 export default Navbar
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,10 +6,10 @@ const Footer = () => {
     <footer classname='footer'>
       <p className='copyright'>Copyright &#169; 2024 Dimitrie Lauer.</p>
 
-      <a href="https://github.com/ItsDimitrie"><i id='firstIcon' class="fa-brands fa-github fa-2xl"></i></a>
-      <a href="https://www.linkedin.com"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
-      <a href="https://twitter.com/_Its_Dimah"><i class="fa-brands fa-x-twitter fa-2xl"></i></a>
-      <a href="https://www.instagram.com/its_dimah/"><i class="fa-brands fa-instagram fa-2xl"></i></a>
+      <a className='socialLink' href="https://github.com/ItsDimitrie"><i id='firstIcon' class="fa-brands fa-github fa-2xl"></i></a>
+      <a className='socialLink' href="https://www.linkedin.com"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+      <a className='socialLink' href="https://twitter.com/_Its_Dimah"><i class="fa-brands fa-x-twitter fa-2xl"></i></a>
+      <a className='socialLink' href="https://www.instagram.com/its_dimah/"><i class="fa-brands fa-instagram fa-2xl"></i></a>
       <p className='email'>dimitrielauer@hotmail.com</p>
 
     </footer>
