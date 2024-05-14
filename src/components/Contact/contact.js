50b3e00ecc6cb683903c04eddfc38e3edf67b1f8
ItsDimitrie <laue0002@HZ.nl>
2024-05-14 21:01:57 +0200

    Added form validation for contact form.

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,28 +1,42 @@
-import React, {useRef} from "react";
+import React, { useRef, useState } from "react";
 import emailjs from '@emailjs/browser';
 import './contact.css';
 
 const Contact = () => {
 
   const form = useRef();
+  const [formError, setFormError] = useState(false);
+
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm('service_c36shpa', 'template_vjtrd2q', form.current, {
-        publicKey: 'lxbKqSg9hGJZ9E4KJ',
-      })
-      .then(
-        () => {
-          console.log('SUCCESS!');
-          e.target.reset();
-          alert('Your message has been sent successfully!');
-        },
-        (error) => {
-          console.log('FAILED...', error.text);
-          alert('Your message could not be sent. Please try again later or contact the email below.');
-        },
-      );
+    const formData = new FormData(form.current);
+    let isValid = true;
+    formData.forEach((value) => {
+      if (!value) {
+        isValid = false;
+      }
+    });
+
+    if (isValid) {
+      emailjs
+        .sendForm('service_c36shpa', 'template_vjtrd2q', form.current, {
+          publicKey: 'lxbKqSg9hGJZ9E4KJ',
+        })
+        .then(
+          () => {
+            console.log('SUCCESS!');
+            e.target.reset();
+            alert('Your message has been sent successfully!');
+          },
+          (error) => {
+            console.log('FAILED...', error.text);
+            alert('Your message could not be sent. Please try again later or contact the email below.');
+          },
+        );
+    } else {
+      setFormError(true);
+    }
   };
 
   return (
@@ -35,7 +49,8 @@ const Contact = () => {
           <input type='text' placeholder='Your Name' className='name' name='your_name' />
           <input type='email' placeholder='Your Email' className='email' name='from_email' />
           <textarea className='msg' name='message' rows='5' placeholder="Your Message"></textarea>
-          <button type='submit' value='send' className='submitBtn'><i id='sendIcon' class="fa-solid fa-paper-plane fa-beat fa-2xl"></i>Submit</button>
+          {formError && <p className="error-msg">Please fill in all fields</p>}
+          <button type='submit' value='send' className='submitBtn'><i id='sendIcon' className="fa-solid fa-paper-plane fa-beat fa-2xl"></i>Submit</button>
           <div className='links'>
           </div>
         </form>
