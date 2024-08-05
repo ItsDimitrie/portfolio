5baa4263a2fdfe61b5a3d1b1c77f48d2abd090c1
ItsDimitrie <laue0002@HZ.nl>
2024-08-05 12:52:02 +0200

    Button to portfolio on home page.

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -10,7 +10,7 @@ const Intro = () => {
         <span className='Hello'>Hello,</span>
         <span className='IntroText'>I'm <span className="introName">Dimitrie</span> <br /> Frontend Developer In Training</span>
         <p className='introPara'>I am a student at the HZ University of Applied Sciences and <br></br> graduated in AV-Specialism at the Curio College</p>
-        <Link><button className="btn"> <i id="workIcon" class="fa-solid fa-briefcase fa-2xl"></i>Check out my work</button></Link>
+        <a href='/portfolio'><button className="btn"> <i id="workIcon" class="fa-solid fa-briefcase fa-2xl"></i>Check out my work</button></a>
        </div>
 
        <img src={bg} alt="Profile" className="bg"/>
